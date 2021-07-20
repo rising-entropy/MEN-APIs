4a77e85510e95a29416a15882d893abdbef9dbf0
unknown <risingentropy20@gmail.com>
2021-07-20 22:09:37 +0530

    post

--- nodeExpressAPI/index.js
+++ nodeExpressAPI/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser'
-import usersRoutes from './routes/users.js'
 
 const app = express();
 const PORT = 5000;
@@ -9,10 +8,27 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use('/users', usersRoutes);
 
 app.listen(PORT, () => console.log(`Server running on Port http://localhost:${PORT}`));
 
 app.get('/', (req, res) => {
     res.send('Hello There!')
+})
+
+const users = [{
+    "fName": "John",
+    "lName": "Doe",
+    "age": 25
+}, {
+    "fName": "Jill",
+    "lName": "Doe",
+    "age": 22
+}]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+app.post('/users', (req, res) => {
+    console.log('yo')
+    res.send("Its Done")
 })
--- nodeExpressAPI/routes/users.js
+++ nodeExpressAPI/routes/users.js
@@ -2,6 +2,22 @@ import express from 'express'
 
 const router = express.Router();
 
-export default router.get('/', (req, res) => {
-    res.send('All Users')
-})
+const users = [{
+    "fName": "John",
+    "lName": "Doe",
+    "age": 25
+}, {
+    "fName": "Jill",
+    "lName": "Doe",
+    "age": 22
+}]
+
+
+export default router.route('/')
+    .get((req, res) => {
+        res.send(users)
+    })
+    .post((req, res) => {
+        console.log(req)
+        res.send("Done")
+    })
